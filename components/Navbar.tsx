ddb469b1ccb88dd3524c195eb94d26d74edd849a
David Wiese <21321101+davidwiese@users.noreply.github.com>
2024-03-18 10:30:47 -0700

    Update alt text for logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -39,7 +39,7 @@ const Navbar = () => {
 					<div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
 						{/* <!-- Logo --> */}
 						<Link className="flex flex-shrink-0 items-center" href="/">
-							<Image className="h-10 w-auto" src={logo} alt="PropertyPulse" />
+							<Image className="h-10 w-auto" src={logo} alt="Trip Report" />
 
 							<span className="hidden md:block text-white text-2xl font-bold ml-2">
 								Trip Report
