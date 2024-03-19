c798a0576bf9d9b30df5c28ac107b71de10e996e
David Wiese <21321101+davidwiese@users.noreply.github.com>
2024-03-19 10:47:38 -0700

    Fix aria labels for a11y

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -22,7 +22,7 @@ const Navbar = () => {
 							id="mobile-dropdown-button"
 							className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
 							aria-controls="mobile-menu"
-							aria-expanded="false"
+							aria-expanded={isMobileMenuOpen}
 						>
 							<span className="absolute -inset-0.5"></span>
 							<span className="sr-only">Open main menu</span>
@@ -123,7 +123,7 @@ const Navbar = () => {
 									type="button"
 									className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
 									id="user-menu-button"
-									aria-expanded="false"
+									aria-expanded={isProfileMenuOpen}
 									aria-haspopup="true"
 									onClick={() => setIsProfileMenuOpen((prev) => !prev)}
 								>
