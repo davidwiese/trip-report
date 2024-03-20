28b4bd010b8adbf2e861d0d95d581e9fe2ee2017
David Wiese <21321101+davidwiese@users.noreply.github.com>
2024-03-20 14:59:32 -0700

    Add type checking and fetch data from db for report properties

---
+++ components/ReportDetails.tsx
@@ -0,0 +1,102 @@
+import {
+	FaBed,
+	FaBath,
+	FaRulerCombined,
+	FaTimes,
+	FaCheck,
+	FaMapMarker,
+} from "react-icons/fa";
+import { Report } from "@/types";
+
+type ReportDetailsProps = {
+	report: Report;
+};
+
+const ReportDetails: React.FC<ReportDetailsProps> = ({ report }) => {
+	return (
+		<main>
+			<div className="bg-white p-6 rounded-lg shadow-md text-center md:text-left">
+				<div className="text-gray-500 mb-4">{report.type}</div>
+				<h1 className="text-3xl font-bold mb-4">{report.name}</h1>
+				<div className="text-gray-500 mb-4 flex align-middle justify-center md:justify-start">
+					<FaMapMarker className="text-lg text-orange-700 mr-2" />
+					<p className="text-orange-700">
+						{report.location.street}, {report.location.city}{" "}
+						{report.location.state}
+					</p>
+				</div>
+
+				<h3 className="text-lg font-bold my-6 bg-gray-800 text-white p-2">
+					Rates & Options
+				</h3>
+				<div className="flex flex-col md:flex-row justify-around">
+					<div className="flex items-center justify-center mb-4 border-b border-gray-200 md:border-b-0 pb-4 md:pb-0">
+						<div className="text-gray-500 mr-2 font-bold">Nightly</div>
+						<div className="text-2xl font-bold text-blue-500">
+							{report.rates.nightly ? (
+								`$${report.rates.nightly.toLocaleString()}`
+							) : (
+								<FaTimes className="text-red-700" />
+							)}
+						</div>
+					</div>
+					<div className="flex items-center justify-center mb-4 border-b border-gray-200 md:border-b-0 pb-4 md:pb-0">
+						<div className="text-gray-500 mr-2 font-bold">Weekly</div>
+						<div className="text-2xl font-bold text-blue-500">
+							{report.rates.weekly ? (
+								`$${report.rates.weekly.toLocaleString()}`
+							) : (
+								<FaTimes className="text-red-700" />
+							)}
+						</div>
+					</div>
+					<div className="flex items-center justify-center mb-4 pb-4 md:pb-0">
+						<div className="text-gray-500 mr-2 font-bold">Monthly</div>
+						<div className="text-2xl font-bold text-blue-500">
+							{report.rates.monthly ? (
+								`$${report.rates.monthly.toLocaleString()}`
+							) : (
+								<FaTimes className="text-red-700" />
+							)}
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div className="bg-white p-6 rounded-lg shadow-md mt-6">
+				<h3 className="text-lg font-bold mb-6">Description & Details</h3>
+				<div className="flex justify-center gap-4 text-blue-500 mb-4 text-xl space-x-9">
+					<p>
+						<FaBed className="inline-block mr-2" /> {report.beds}{" "}
+						<span className="hidden sm:inline">Beds</span>
+					</p>
+					<p>
+						<FaBath className="inline-block mr-2" /> {report.baths}{" "}
+						<span className="hidden sm:inline">Baths</span>
+					</p>
+					<p>
+						<FaRulerCombined className="inline-block mr-2" />{" "}
+						{report.square_feet} <span className="hidden sm:inline">sqft</span>
+					</p>
+				</div>
+				<p className="text-gray-500 mb-4 text-center">{report.description}</p>
+			</div>
+
+			<div className="bg-white p-6 rounded-lg shadow-md mt-6">
+				<h3 className="text-lg font-bold mb-6">Amenities</h3>
+
+				<ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 list-none space-y-2">
+					{report.amenities.map((amenity: string, index: number) => (
+						<li key={index}>
+							<FaCheck className="text-green-600 mr-2 inline-block" /> {amenity}
+						</li>
+					))}
+				</ul>
+			</div>
+			<div className="bg-white p-6 rounded-lg shadow-md mt-6">
+				<div id="map"></div>
+			</div>
+		</main>
+	);
+};
+export default ReportDetails;
