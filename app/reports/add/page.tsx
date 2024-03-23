adfe0a7ed5dbe37ca423d8dd60ad38541ef01ff9
David Wiese <21321101+davidwiese@users.noreply.github.com>
2024-03-23 10:32:37 -0700

    Start report input form

--- app/reports/add/page.tsx
+++ app/reports/add/page.tsx
@@ -1,8 +1,18 @@
+import ReportAddForm from "@/components/ReportAddForm";
+
 type ReportAddPageProps = {
 	// Add any props here if needed
 };
 
 const ReportAddPage: React.FC<ReportAddPageProps> = () => {
-	return <div>ReportAddPage</div>;
+	return (
+		<section className="bg-blue-50">
+			<div className="container m-auto max-w-2xl py-24">
+				<div className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0">
+					<ReportAddForm />
+				</div>
+			</div>
+		</section>
+	);
 };
 export default ReportAddPage;
