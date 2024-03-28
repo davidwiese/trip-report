9f758ab3b91e5325b94c82c91cc2c19d565533b4
David Wiese <21321101+davidwiese@users.noreply.github.com>
2024-03-28 10:21:44 -0700

    Create profile page

---
+++ app/profile/page.tsx
@@ -0,0 +1,8 @@
+type ProfilePageProps = {
+	// Add any props here if needed
+};
+
+const ProfilePage: React.FC<ProfilePageProps> = () => {
+	return <div>ProfilePage</div>;
+};
+export default ProfilePage;
