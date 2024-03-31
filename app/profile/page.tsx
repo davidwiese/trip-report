a32e0488f7ddeb5b683be9bd5151674c427994b1
David Wiese <21321101+davidwiese@users.noreply.github.com>
2024-03-31 11:07:55 -0700

    Define Report type

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,18 +1,61 @@
 "use client";
+import { useState, useEffect } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { useSession } from "next-auth/react";
+import { Session } from "next-auth";
 import profileDefault from "@/assets/images/profile.png";
+import Spinner from "@/components/Spinner";
 
 type ProfilePageProps = {
 	// Add any props here if needed
 };
+
+// Extend the Session type to include the user property with an id field
+interface CustomSession extends Session {
+	user?: {
+		id?: string;
+		name?: string | null;
+		email?: string | null;
+		image?: string | null;
+	};
+}
 
 const ProfilePage: React.FC<ProfilePageProps> = () => {
 	const { data: session } = useSession();
 	const profileImage = session?.user?.image;
 	const profileName = session?.user?.name;
 	const profileEmail = session?.user?.email;
+
+	const [reports, setReports] = useState([]);
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		const fetchUserReports = async (userId: string) => {
+			if (!userId) {
+				return;
+			}
+
+			try {
+				const res = await fetch(`/api/reports/user/${userId}`);
+				if (res.status === 200) {
+					const data = await res.json();
+					setReports(data);
+				}
+			} catch (error) {
+				console.log(error);
+			} finally {
+				setLoading(false);
+			}
+		};
+
+		// Fetch user reports when session is available
+		if (session?.user?.id) {
+			fetchUserReports(session.user.id);
+		}
+	}, [session]);
+
+	const handleDeleteReport = () => {};
 
 	return (
 		<section className="bg-blue-50">
@@ -40,64 +83,49 @@ const ProfilePage: React.FC<ProfilePageProps> = () => {
 
 						<div className="md:w-3/4 md:pl-4">
 							<h2 className="text-xl font-semibold mb-4">Your Listings</h2>
-							<div className="mb-10">
-								<Link href="/property.html">
-									<Image
-										className="h-32 w-full rounded-md object-cover"
-										src="/images/properties/a1.jpg"
-										alt="Property 1"
-										width={0}
-										height={0}
-									/>
-								</Link>
-								<div className="mt-2">
-									<p className="text-lg font-semibold">Property Title 1</p>
-									<p className="text-gray-600">Address: 123 Main St</p>
-								</div>
-								<div className="mt-2">
-									<a
-										href="/add-property.html"
-										className="bg-blue-500 text-white px-3 py-3 rounded-md mr-2 hover:bg-blue-600"
-									>
-										Edit
-									</a>
-									<button
-										className="bg-red-500 text-white px-3 py-2 rounded-md hover:bg-red-600"
-										type="button"
-									>
-										Delete
-									</button>
-								</div>
-							</div>
-							<div className="mb-10">
-								<a href="/property.html">
-									<Image
-										className="h-32 w-full rounded-md object-cover"
-										src="/images/properties/b1.jpg"
-										alt="Property 2"
-										width={0}
-										height={0}
-									/>
-								</a>
-								<div className="mt-2">
-									<p className="text-lg font-semibold">Property Title 2</p>
-									<p className="text-gray-600">Address: 456 Elm St</p>
-								</div>
-								<div className="mt-2">
-									<a
-										href="/add-property.html"
-										className="bg-blue-500 text-white px-3 py-3 rounded-md mr-2 hover:bg-blue-600"
-									>
-										Edit
-									</a>
-									<button
-										className="bg-red-500 text-white px-3 py-2 rounded-md hover:bg-red-600"
-										type="button"
-									>
-										Delete
-									</button>
-								</div>
-							</div>
+							{!loading && reports.length === 0 && (
+								<p>You have no trip reports</p>
+							)}
+							{loading ? (
+								<Spinner loading={loading} />
+							) : (
+								reports.map((report) => (
+									<div key={report._id} className="mb-10">
+										<Link href={`/reports/${report._id}`}>
+											<Image
+												className="h-32 w-full rounded-md object-cover"
+												src={report.images[0]}
+												alt=""
+												width={500}
+												height={100}
+												priority={true}
+											/>
+										</Link>
+										<div className="mt-2">
+											<p className="text-lg font-semibold">{report.name}</p>
+											<p className="text-gray-600">
+												Address: {report.location.street} {report.location.city}{" "}
+												{report.location.state}
+											</p>
+										</div>
+										<div className="mt-2">
+											<Link
+												href={`/reports/${report._id}/edit`}
+												className="bg-blue-500 text-white px-3 py-3 rounded-md mr-2 hover:bg-blue-600"
+											>
+												Edit
+											</Link>
+											<button
+												onClick={() => handleDeleteReport(report._id)}
+												className="bg-red-500 text-white px-3 py-2 rounded-md hover:bg-red-600"
+												type="button"
+											>
+												Delete
+											</button>
+										</div>
+									</div>
+								))
+							)}
 						</div>
 					</div>
 				</div>
