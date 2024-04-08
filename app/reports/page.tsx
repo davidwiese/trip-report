f38cfd99adea66247f30e87c31fa2d8295b83bfb
David Wiese <21321101+davidwiese@users.noreply.github.com>
2024-04-08 11:34:33 -0700

    Finish search implementation

--- app/reports/page.tsx
+++ app/reports/page.tsx
@@ -1,6 +1,7 @@
 import ReportCard from "@/components/ReportCard";
 import { Report } from "@/types";
 import { fetchReports } from "@/utils/requests";
+import ReportSearchForm from "@/components/ReportSearchForm";
 
 type ReportsPageProps = {
 	// Add any props here if needed
@@ -16,19 +17,27 @@ const ReportsPage: React.FC<ReportsPageProps> = async () => {
 	);
 
 	return (
-		<section className="px-4 py-6">
-			<div className="container-xl lg:container m-auto px-4 py-6">
-				{reports.length === 0 ? (
-					<p>No reports found</p>
-				) : (
-					<div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-						{(reports as Report[]).map((report) => (
-							<ReportCard key={report._id} report={report} />
-						))}
-					</div>
-				)}
-			</div>
-		</section>
+		<>
+			<section className="bg-blue-700 py-4">
+				<div className="max-w-7xl mx-auto px-4 flex flex-col items-start sm:px-6 lg:px-8">
+					<ReportSearchForm />
+				</div>
+			</section>
+
+			<section className="px-4 py-6">
+				<div className="container-xl lg:container m-auto px-4 py-6">
+					{reports.length === 0 ? (
+						<p>No reports found</p>
+					) : (
+						<div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+							{(reports as Report[]).map((report) => (
+								<ReportCard key={report._id} report={report} />
+							))}
+						</div>
+					)}
+				</div>
+			</section>
+		</>
 	);
 };
 export default ReportsPage;
--- app/reports/search-results/page.tsx
+++ app/reports/search-results/page.tsx
@@ -1,6 +1,12 @@
 "use client";
 import { useState, useEffect } from "react";
 import { useSearchParams } from "next/navigation";
+import Link from "next/link";
+import { FaArrowAltCircleLeft } from "react-icons/fa";
+import ReportCard from "@/components/ReportCard";
+import Spinner from "@/components/Spinner";
+import { Report } from "@/types";
+import ReportSearchForm from "@/components/ReportSearchForm";
 
 type SearchResultsPageProps = {
 	// Add any props here if needed
@@ -35,6 +41,38 @@ const SearchResultsPage: React.FC<SearchResultsPageProps> = () => {
 		fetchSearchResults();
 	}, [location, reportType]);
 
-	return <div>SearchResultsPage</div>;
+	return (
+		<>
+			<section className="bg-blue-700 py-4">
+				<div className="max-w-7xl mx-auto px-4 flex flex-col items-start sm:px-6 lg:px-8">
+					<ReportSearchForm />
+				</div>
+			</section>
+			{loading ? (
+				<Spinner loading={loading} />
+			) : (
+				<section className="px-4 py-6">
+					<div className="container-xl lg:container m-auto px-4 py-6">
+						<Link
+							href="/reports"
+							className="flex items-center text-blue-500 hover:underline mb-3"
+						>
+							<FaArrowAltCircleLeft className="mr-2 mb-1" /> Back To Reports
+						</Link>
+						<h1 className="text-2xl mb-4">Search Results</h1>
+						{reports.length === 0 ? (
+							<p>No search results found</p>
+						) : (
+							<div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+								{(reports as Report[]).map((report) => (
+									<ReportCard key={report._id} report={report} />
+								))}
+							</div>
+						)}
+					</div>
+				</section>
+			)}
+		</>
+	);
 };
 export default SearchResultsPage;
