828b00b9c46de405a6ee932b14d0e4f0fc30d023
David Wiese <21321101+davidwiese@users.noreply.github.com>
2024-04-10 15:03:11 -0700

    Add message delete api

--- app/api/messages/[id]/route.ts
+++ app/api/messages/[id]/route.ts
@@ -45,3 +45,41 @@ export const PUT = async (
 		return new Response("Something went wrong", { status: 500 });
 	}
 };
+
+// DELETE /api/message/:id
+export const DELETE = async (
+	request: NextRequest,
+	{ params }: { params: { id: string } }
+) => {
+	try {
+		await connectDB();
+
+		const { id } = params;
+
+		const sessionUser = await getSessionUser();
+
+		if (!sessionUser || !sessionUser.user) {
+			return new Response("User ID is required", {
+				status: 401,
+			});
+		}
+
+		const { userId } = sessionUser;
+
+		const message = await Message.findById(id);
+
+		if (!message) return new Response("Message not found", { status: 404 });
+
+		// Verify ownership
+		if (message.recipient.toString() !== userId) {
+			return new Response("Unauthorized", { status: 401 });
+		}
+
+		await message.deleteOne();
+
+		return new Response("Message Deleted", { status: 200 });
+	} catch (error) {
+		console.log(error);
+		return new Response("Something went wrong", { status: 500 });
+	}
+};
