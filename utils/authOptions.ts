b11d4edb278144724067b57a599766ffe32c6341
David Wiese <21321101+davidwiese@users.noreply.github.com>
2024-04-12 10:17:19 -0700

    Fix type errors

--- utils/authOptions.ts
+++ utils/authOptions.ts
@@ -1,11 +1,20 @@
 import connectDB from "@/config/database";
 import User from "@/models/User";
-import { Profile, Session } from "next-auth";
+import { Profile, Session, Account, User as NextAuthUser } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { Types } from "mongoose";
 
 // Create a custom type that extends the Profile type
 interface GoogleProfile extends Profile {
 	picture: string;
+}
+
+// Create an interface that represents the User model
+interface UserDoc {
+	email: string;
+	username: string;
+	image?: string;
+	bookmarks?: Types.ObjectId[];
 }
 
 export const authOptions = {
@@ -24,20 +33,36 @@ export const authOptions = {
 	],
 	callbacks: {
 		// Invoked on successful sign in
-		async signIn({ profile }: { profile: GoogleProfile }) {
+		async signIn({
+			user,
+			account,
+			profile,
+			email,
+			credentials,
+		}: {
+			user: NextAuthUser;
+			account: Account | null;
+			profile?: Profile | undefined;
+			email?: {
+				verificationRequest?: boolean;
+			};
+			credentials?: Record<string, unknown>;
+		}) {
 			// Connect to DB
 			await connectDB();
 			// Check if user exists
-			const userExists = await User.findOne({ email: profile.email });
+			const userExists = await User.findOne({
+				email: (profile as GoogleProfile)?.email,
+			});
 			// If not, add user to DB
 			if (!userExists) {
 				// Truncate user name if too long
-				const username = profile.name?.slice(0, 20);
+				const username = (profile as GoogleProfile)?.name?.slice(0, 20);
 
 				await User.create({
-					email: profile.email,
+					email: (profile as GoogleProfile)?.email,
 					username,
-					image: profile.picture,
+					image: (profile as GoogleProfile)?.picture,
 				});
 			}
 			// Return true to allow sign in
@@ -50,7 +75,7 @@ export const authOptions = {
 			const user = await User.findOne({ email: session.user?.email });
 			// Assign user id to session
 			if (user && session.user) {
-				(session.user as any).id = user._id.toString();
+				(session.user as unknown as any).id = user._id.toString();
 			}
 			// Return session
 			return session;
