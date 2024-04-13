4a2620bf9e3f9fddb941247050dde063d4a57611
David Wiese <21321101+davidwiese@users.noreply.github.com>
2024-04-13 10:44:09 -0700

    Update copy

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -10,7 +10,7 @@ const Hero: React.FC<HeroProps> = () => {
 			<div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center">
 				<div className="text-center">
 					<h1 className="text-4xl font-extrabold text-white sm:text-5xl md:text-6xl">
-						Find The Perfect Rental
+						Find Your Next Adventure
 					</h1>
 					<p className="my-4 text-xl text-white">
 						Discover the perfect objective for your next trip.
