81d7d629e4bea233b7b289bfba6091f53b8b227c
David Wiese <21321101+davidwiese@users.noreply.github.com>
2024-04-15 11:45:38 -0700

    Update protected routes

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,11 @@
 export { default } from "next-auth/middleware";
 export const config = {
-	matcher: ["/reports/add", "/profile", "/reports/saved", "/messages"],
+	matcher: [
+		"/reports/add",
+		// NOTE: editing a property should be a private route
+		"/reports/:id/edit",
+		"/profile",
+		"/reports/saved",
+		"/messages",
+	],
 };
