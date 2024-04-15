2ef2aeab11ffbf40c3da76d992fa71efa692f08f
David Wiese <21321101+davidwiese@users.noreply.github.com>
2024-04-15 15:53:52 -0700

    Query DB directly with server component, remove fetch

--- components/FeaturedReports.tsx
+++ components/FeaturedReports.tsx
@@ -1,31 +1,33 @@
-import { fetchReports } from "@/utils/requests";
 import FeaturedReportCard from "@/components/FeaturedReportCard";
-import { Report } from "@/types";
+import connectDB from "@/config/database";
+import { Report as ReportType } from "@/types";
+import Report from "@/models/Report";
 
 type FeaturedReportsProps = {
 	// Add any props here if needed
 };
 
 const FeaturedReports: React.FC<FeaturedReportsProps> = async () => {
-	const reports = await fetchReports({
-		showFeatured: true,
-	});
+	await connectDB();
 
-	return (
-		reports.length > 0 && (
-			<section className="bg-blue-50 px-4 pt-6 pb-10">
-				<div className="container-xl lg:container m-auto">
-					<h2 className="text-3xl font-bold text-blue-500 mb-6 text-center">
-						Featured Reports
-					</h2>
-					<div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-						{reports.map((report: Report) => (
-							<FeaturedReportCard key={report._id} report={report} />
-						))}
-					</div>
+	const reports: ReportType[] = await Report.find({
+		isFeatured: true,
+	}).lean();
+
+	return reports.length > 0 ? (
+		<section className="bg-blue-50 px-4 pt-6 pb-10">
+			<div className="container-xl lg:container m-auto">
+				<h2 className="text-3xl font-bold text-blue-500 mb-6 text-center">
+					Featured Reports
+				</h2>
+				<div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+					{reports.map((report: ReportType) => (
+						<FeaturedReportCard key={report._id} report={report} />
+					))}
 				</div>
-			</section>
-		)
-	);
+			</div>
+		</section>
+	) : null;
 };
+
 export default FeaturedReports;
