908089fb29f680f5432a102ef4d419f280af494b
David Wiese <21321101+davidwiese@users.noreply.github.com>
2024-04-16 11:35:09 -0700

    Make server component and remove unnecessary component

--- app/messages/page.tsx
+++ app/messages/page.tsx
@@ -1,10 +1,71 @@
-import Messages from "@/components/Messages";
+import MessageCard from "@/components/Message";
+import connectDB from "@/config/database";
+import Message from "@/models/Message";
+import { getSessionUser } from "@/utils/getSessionUser";
+import { Message as MessageType } from "@/types";
 
 type MessagesPageProps = {
 	// Add any props here if needed
 };
 
-const MessagesPage: React.FC<MessagesPageProps> = () => {
-	return <Messages />;
+const MessagePage: React.FC<MessagesPageProps> = async () => {
+	await connectDB();
+
+	const sessionUser = await getSessionUser();
+
+	if (!sessionUser) {
+		return (
+			<section className="bg-blue-50">
+				<div className="container m-auto py-24 max-w-6xl">
+					<div className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0">
+						<h1 className="text-3xl font-bold mb-4">Your Messages</h1>
+						<p>You are not logged in</p>
+					</div>
+				</div>
+			</section>
+		);
+	}
+
+	const { userId } = sessionUser;
+
+	const readMessages = await Message.find({ recipient: userId, read: true })
+		.sort({ createdAt: -1 }) // Sort read messages in asc order
+		.populate("sender", "username")
+		.populate("report", "name")
+		.lean();
+
+	const unreadMessages = await Message.find({
+		recipient: userId,
+		read: false,
+	})
+		.sort({ createdAt: -1 }) // Sort read messages in asc order
+		.populate("sender", "username")
+		.populate("report", "name")
+		.lean();
+
+	const messages = [...unreadMessages, ...readMessages];
+
+	return (
+		<section className="bg-blue-50">
+			<div className="container m-auto py-24 max-w-6xl">
+				<div className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0">
+					<h1 className="text-3xl font-bold mb-4">Your Messages</h1>
+
+					<div className="space-y-4">
+						{messages.length === 0 ? (
+							<p>You have no messages</p>
+						) : (
+							messages.map((message) => (
+								<MessageCard
+									key={message._id as string}
+									message={message as MessageType}
+								/>
+							))
+						)}
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 };
-export default MessagesPage;
+export default MessagePage;
