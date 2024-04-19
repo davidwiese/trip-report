fe67f4042675adbc5680f3cbac8153ea5e340e18
David Wiese <21321101+davidwiese@users.noreply.github.com>
2024-04-19 12:48:10 -0700

    Try updated getSessionUser

--- utils/getSessionUser.ts
+++ utils/getSessionUser.ts
@@ -1,20 +1,33 @@
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "@/utils/authOptions";
 
+interface SessionUser {
+	id: string;
+}
+
 export const getSessionUser = async () => {
 	try {
 		const session = await getServerSession(authOptions);
 
 		if (!session || !session.user) {
-			return null;
+			throw new Error("No session or user found");
 		}
 
+		const expiresAt = new Date(session.expires);
+		const currentTime = new Date();
+
+		if (expiresAt < currentTime) {
+			throw new Error("Session has expired");
+		}
+
+		const user = session.user as SessionUser;
+
 		return {
-			user: session.user,
-			userId: (session.user as { id: string }).id,
+			user,
+			userId: user.id,
 		};
 	} catch (error) {
-		console.error(error);
-		return null;
+		console.error("Error in getSessionUser:", error);
+		throw error;
 	}
 };
