f2b8c01082521c5a2df7ea539a893aecf66f93b2
David Wiese <21321101+davidwiese@users.noreply.github.com>
2024-05-02 11:20:51 -0700

    Remove map/mapbox, adjust report details to remove errors for now

--- components/ReportDetails.tsx
+++ components/ReportDetails.tsx
@@ -7,7 +7,6 @@ import {
 	FaMapMarker,
 } from "react-icons/fa";
 import { Report } from "@/types";
-import ReportMap from "@/components/ReportMap";
 
 type ReportDetailsProps = {
 	report: Report;
@@ -17,13 +16,12 @@ const ReportDetails: React.FC<ReportDetailsProps> = ({ report }) => {
 	return (
 		<main>
 			<div className="bg-white p-6 rounded-lg shadow-md text-center md:text-left">
-				<div className="text-gray-500 mb-4">{report.type}</div>
-				<h1 className="text-3xl font-bold mb-4">{report.name}</h1>
+				<div className="text-gray-500 mb-4">{report.title}</div>
+				<h1 className="text-3xl font-bold mb-4">{report.description}</h1>
 				<div className="text-gray-500 mb-4 flex align-middle justify-center md:justify-start">
 					<FaMapMarker className="text-lg text-orange-700 mr-2" />
 					<p className="text-orange-700">
-						{report.location.street}, {report.location.city}{" "}
-						{report.location.state}
+						{report.location}, {report.location} {report.location}
 					</p>
 				</div>
 
@@ -33,33 +31,6 @@ const ReportDetails: React.FC<ReportDetailsProps> = ({ report }) => {
 				<div className="flex flex-col md:flex-row justify-around">
 					<div className="flex items-center justify-center mb-4 border-b border-gray-200 md:border-b-0 pb-4 md:pb-0">
 						<div className="text-gray-500 mr-2 font-bold">Nightly</div>
-						<div className="text-2xl font-bold text-blue-500">
-							{report.rates.nightly ? (
-								`$${report.rates.nightly.toLocaleString()}`
-							) : (
-								<FaTimes className="text-red-700" />
-							)}
-						</div>
-					</div>
-					<div className="flex items-center justify-center mb-4 border-b border-gray-200 md:border-b-0 pb-4 md:pb-0">
-						<div className="text-gray-500 mr-2 font-bold">Weekly</div>
-						<div className="text-2xl font-bold text-blue-500">
-							{report.rates.weekly ? (
-								`$${report.rates.weekly.toLocaleString()}`
-							) : (
-								<FaTimes className="text-red-700" />
-							)}
-						</div>
-					</div>
-					<div className="flex items-center justify-center mb-4 pb-4 md:pb-0">
-						<div className="text-gray-500 mr-2 font-bold">Monthly</div>
-						<div className="text-2xl font-bold text-blue-500">
-							{report.rates.monthly ? (
-								`$${report.rates.monthly.toLocaleString()}`
-							) : (
-								<FaTimes className="text-red-700" />
-							)}
-						</div>
 					</div>
 				</div>
 			</div>
@@ -68,16 +39,16 @@ const ReportDetails: React.FC<ReportDetailsProps> = ({ report }) => {
 				<h3 className="text-lg font-bold mb-6">Description & Details</h3>
 				<div className="flex justify-center gap-4 text-blue-500 mb-4 text-xl space-x-9">
 					<p>
-						<FaBed className="inline-block mr-2" /> {report.beds}{" "}
+						<FaBed className="inline-block mr-2" /> {report.title}{" "}
 						<span className="hidden sm:inline">Beds</span>
 					</p>
 					<p>
-						<FaBath className="inline-block mr-2" /> {report.baths}{" "}
+						<FaBath className="inline-block mr-2" /> {report.title}{" "}
 						<span className="hidden sm:inline">Baths</span>
 					</p>
 					<p>
-						<FaRulerCombined className="inline-block mr-2" />{" "}
-						{report.square_feet} <span className="hidden sm:inline">sqft</span>
+						<FaRulerCombined className="inline-block mr-2" /> {report.title}{" "}
+						<span className="hidden sm:inline">sqft</span>
 					</p>
 				</div>
 				<p className="text-gray-500 mb-4 text-center">{report.description}</p>
@@ -87,15 +58,12 @@ const ReportDetails: React.FC<ReportDetailsProps> = ({ report }) => {
 				<h3 className="text-lg font-bold mb-6">Amenities</h3>
 
 				<ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 list-none space-y-2">
-					{report.amenities.map((amenity: string, index: number) => (
+					{report.activityType.map((amenity: string, index: number) => (
 						<li key={index}>
 							<FaCheck className="text-green-600 mr-2 inline-block" /> {amenity}
 						</li>
 					))}
 				</ul>
-			</div>
-			<div className="bg-white p-6 rounded-lg shadow-md mt-6">
-				<ReportMap report={report} />
 			</div>
 		</main>
 	);
