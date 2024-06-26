edbd01bcc91efa637e4f7018a842fc6479ffc6ef
David Wiese <21321101+davidwiese@users.noreply.github.com>
2024-06-26 14:45:57 -0700

    Fix button width

--- components/DownloadButton.tsx
+++ components/DownloadButton.tsx
@@ -19,7 +19,7 @@ const DownloadButton: React.FC<DownloadButtonProps> = ({ report }) => {
 				href={`/api/download?url=${encodeURIComponent(
 					report.gpxFile.url
 				)}&filename=${encodeURIComponent(originalFilename)}`}
-				className="flex items-center"
+				className="flex items-center justify-center w-[183px]"
 			>
 				<RxDownload className="mr-2 text-xl" />
 				Download GPX File
