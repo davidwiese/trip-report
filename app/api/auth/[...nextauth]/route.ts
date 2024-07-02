4897f0e05e15556a759614e0c673c0b22641246e
David Wiese <21321101+davidwiese@users.noreply.github.com>
2024-07-02 12:13:48 -0700

    Undo auth rate limiting since will be using Clerk

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,23 +1,6 @@
 import { authOptions } from "@/utils/authOptions";
 import NextAuth from "next-auth/next";
-import { standardRateLimit } from "@/utils/ratelimit";
 
 const handler = NextAuth(authOptions);
 
-export async function GET(req: Request) {
-	const ip = req.headers.get("x-forwarded-for") ?? "127.0.0.1";
-	const { success } = await standardRateLimit.limit(ip);
-	if (!success) {
-		return new Response("Too many requests", { status: 429 });
-	}
-	return handler(req);
-}
-
-export async function POST(req: Request) {
-	const ip = req.headers.get("x-forwarded-for") ?? "127.0.0.1";
-	const { success } = await standardRateLimit.limit(ip);
-	if (!success) {
-		return new Response("Too many requests", { status: 429 });
-	}
-	return handler(req);
-}
+export { handler as GET, handler as POST };
