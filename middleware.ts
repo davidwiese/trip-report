b6df0fcfd9e4bbe0513c6b03a1dc2fbae6e58323
David Wiese <21321101+davidwiese@users.noreply.github.com>
2024-07-29 11:48:31 -0700

    Update CSP

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,4 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
 
 // Define protected routes
 const isProtectedRoute = createRouteMatcher([
@@ -14,58 +13,8 @@ export default clerkMiddleware((auth, req) => {
 	if (isProtectedRoute(req)) {
 		auth().protect();
 	}
-
-	const nonce = Buffer.from(crypto.randomUUID()).toString("base64");
-	const cspHeader = `
-  default-src 'self';
-  script-src 'self' 'nonce-${nonce}' 'strict-dynamic' https://clerk.accounts.dev https://*.clerk.accounts.dev https://cdn.jsdelivr.net https://js.sentry-cdn.com https://browser.sentry-cdn.com https://*.ingest.sentry.io https://challenges.cloudflare.com https://scdn.clerk.com https://segapi.clerk.com https://app.posthog.com;
-  style-src 'self' 'nonce-${nonce}' https://cdn.jsdelivr.net;
-  img-src 'self' https: data: blob:;
-  font-src 'self' https: data:;
-  object-src 'none';
-  base-uri 'self';
-  form-action 'self';
-  frame-ancestors 'none';
-  frame-src 'self' https://*.clerk.accounts.dev https://app.posthog.com;
-  connect-src 'self' https://*.clerk.accounts.dev https://*.ingest.sentry.io https://app.posthog.com https://api.cloudinary.com https://api.resend.com;
-  media-src 'self' https://res.cloudinary.com;
-  worker-src 'self' blob:;
-  upgrade-insecure-requests;
-`;
-
-	const contentSecurityPolicyHeaderValue = cspHeader
-		.replace(/\s{2,}/g, " ")
-		.trim();
-
-	const requestHeaders = new Headers(req.headers);
-	requestHeaders.set("x-nonce", nonce);
-	requestHeaders.set(
-		"Content-Security-Policy",
-		contentSecurityPolicyHeaderValue
-	);
-
-	const response = NextResponse.next({
-		request: {
-			headers: requestHeaders,
-		},
-	});
-	response.headers.set(
-		"Content-Security-Policy",
-		contentSecurityPolicyHeaderValue
-	);
-
-	return response;
 });
 
 export const config = {
-	matcher: [
-		"/((?!api|_next/static|_next/image|favicon.ico).*)",
-		{
-			source: "/(.*)",
-			missing: [
-				{ type: "header", key: "next-router-prefetch" },
-				{ type: "header", key: "purpose", value: "prefetch" },
-			],
-		},
-	],
+	matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 };
--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,20 @@
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline' https://clerk.accounts.dev https://*.clerk.accounts.dev https://cdn.jsdelivr.net https://js.sentry-cdn.com https://browser.sentry-cdn.com https://*.ingest.sentry.io https://challenges.cloudflare.com https://scdn.clerk.com https://segapi.clerk.com https://app.posthog.com;
+    style-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net;
+    img-src 'self' https: data: blob:;
+    font-src 'self' https: data:;
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    frame-src 'self' https://*.clerk.accounts.dev https://app.posthog.com;
+    connect-src 'self' https://*.clerk.accounts.dev https://*.ingest.sentry.io https://app.posthog.com https://api.cloudinary.com https://api.resend.com;
+    media-src 'self' https://res.cloudinary.com;
+    worker-src 'self' blob:;
+    upgrade-insecure-requests;
+`;
+
 const nextConfig = {
 	images: {
 		remotePatterns: [
@@ -17,6 +34,19 @@ const nextConfig = {
 				pathname: "**",
 			},
 		],
+	},
+	async headers() {
+		return [
+			{
+				source: "/(.*)",
+				headers: [
+					{
+						key: "Content-Security-Policy",
+						value: cspHeader.replace(/\s{2,}/g, " ").trim(),
+					},
+				],
+			},
+		];
 	},
 };
 
