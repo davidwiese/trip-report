ae43098d112e133334ff1a55094d021d53292433
David Wiese <21321101+davidwiese@users.noreply.github.com>
2024-08-16 15:35:01 -0700

    Fix layout maybe

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,8 +36,10 @@ export default function RootLayout({
 		? {
 				signInUrl: process.env.NEXT_PUBLIC_CLERK_SIGN_IN_URL,
 				signUpUrl: process.env.NEXT_PUBLIC_CLERK_SIGN_UP_URL,
-				afterSignInUrl: process.env.NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL,
-				afterSignUpUrl: process.env.NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL,
+				afterSignInUrl:
+					process.env.NEXT_PUBLIC_CLERK_SIGN_IN_FALLBACK_REDIRECT_URL,
+				afterSignUpUrl:
+					process.env.NEXT_PUBLIC_CLERK_SIGN_UP_FALLBACK_REDIRECT_URL,
 		  }
 		: {};
 
