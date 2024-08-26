79477461b23b944abd60453c9e7d0c2696051e1b
David Wiese <21321101+davidwiese@users.noreply.github.com>
2024-08-26 11:03:50 -0700

    Add instagram logo to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,7 @@
 import logo from "@/public/images/logo_fill.png";
 import Image from "next/image";
 import Link from "next/link";
+import { BiLogoInstagramAlt } from "react-icons/bi";
 
 type FooterProps = {};
 
@@ -9,36 +10,48 @@ const Footer: React.FC<FooterProps> = () => {
 
 	return (
 		<footer className="bg-gray-200 py-4 mt-0">
-			<div className="container mx-auto flex flex-col md:flex-row items-center justify-between px-2 sm:px-6 lg:px-8">
-				<div className="mb-4 md:mb-0 flex items-center">
-					<Image src={logo} alt="Logo" className="h-8 w-auto ml-0.5" />
-					<div className="ml-4 flex items-center">
-						<Link
-							href="/privacy-policy"
-							className="text-sm text-gray-600 hover:text-gray-800 hover:underline"
-						>
-							Privacy
-						</Link>
-						<span className="mx-2 text-gray-600">·</span>
-						<Link
-							href="/terms"
-							className="text-sm text-gray-600 hover:text-gray-800 hover:underline"
-						>
-							Terms
-						</Link>
-						<span className="mx-2 text-gray-600">·</span>
-						<Link
-							href="/contact"
-							className="text-sm text-gray-600 hover:text-gray-800 hover:underline"
-						>
-							Contact
-						</Link>
+			<div className="container mx-auto px-2 sm:px-6 lg:px-8">
+				<div className="grid grid-cols-1 md:grid-cols-3 gap-2 items-center">
+					<div className="flex items-center justify-center md:justify-start">
+						<Image src={logo} alt="Logo" className="h-8 w-auto ml-0.5" />
+						<div className="ml-4 flex items-center">
+							<Link
+								href="/privacy-policy"
+								className="text-sm text-gray-600 hover:text-gray-800 hover:underline"
+							>
+								Privacy
+							</Link>
+							<span className="mx-2 text-gray-600">·</span>
+							<Link
+								href="/terms"
+								className="text-sm text-gray-600 hover:text-gray-800 hover:underline"
+							>
+								Terms
+							</Link>
+							<span className="mx-2 text-gray-600">·</span>
+							<Link
+								href="/contact"
+								className="text-sm text-gray-600 hover:text-gray-800 hover:underline"
+							>
+								Contact
+							</Link>
+						</div>
 					</div>
-				</div>
-				<div>
-					<p className="text-sm text-gray-600 mt-2 md:mt-0">
-						&copy; {currentYear} Trip Report™. All rights reserved.
-					</p>
+					<div className="flex justify-center items-center">
+						<a
+							href="https://www.instagram.com/tripreport.co"
+							target="_blank"
+							rel="noopener noreferrer"
+							className="flex justify-center items-center"
+						>
+							<BiLogoInstagramAlt className="h-10 w-auto" />
+						</a>
+					</div>
+					<div className="flex flex-col items-center md:items-end">
+						<p className="text-sm text-gray-600 mt-2 md:mt-0 text-nowrap">
+							&copy; {currentYear} Trip Report™. All rights reserved.
+						</p>
+					</div>
 				</div>
 			</div>
 		</footer>
