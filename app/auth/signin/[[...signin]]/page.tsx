9d682a40b0d4620370de8fbfa66a3789fa5f228f
David Wiese <21321101+davidwiese@users.noreply.github.com>
2024-09-17 15:42:28 -0700

    Add dark mode to signin and signup pages

--- app/auth/signin/[[...signin]]/page.tsx
+++ app/auth/signin/[[...signin]]/page.tsx
@@ -2,7 +2,7 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function SignInPage() {
 	return (
-		<div className="flex items-center justify-center min-h-screen bg-gray-100">
+		<div className="flex flex-col items-center justify-center min-h-screen bg-white dark:bg-black dark:bg-gradient-to-b dark:from-[#191919] dark:via-black dark:to-[#191919]">
 			<SignIn />
 		</div>
 	);
